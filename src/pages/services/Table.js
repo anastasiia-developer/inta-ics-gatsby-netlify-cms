612ef2c1b7bf9569da7f7672c9d0ba321433fc8f
Anastasiia Derymarko <lolololo2263@gmail.com>
2020-03-06 09:45:18 +0200

    table mob

--- src/pages/services/Table.js
+++ src/pages/services/Table.js
@@ -5,10 +5,7 @@ import Time from "../../img/time.svg";
 import ArrowLine from '../../img/arrowLine.svg'
 
 const Wrapper = styled.div`
-    margin: 3em auto;
-    width: 75%;
-    background: #F9FCFF;
-    border: 1px solid #D2D6DD;
+
     border-radius: .5em;
     svg, img{ 
         width: 1em;
@@ -22,12 +19,10 @@ const Wrapper = styled.div`
     }
     tr th{
         color: #005BE4;
-        
     }
     td, th {
       vertical-align: middle;
       text-align: left;
-      border-left: 1px solid #D2D6DD; 
     }
     thead th{
       border-top: none; 
@@ -43,10 +38,7 @@ const Wrapper = styled.div`
         cursor: pointer;
        }
     } 
-    th:first-child, td:first-child{
-      border-left: none; 
-      color: #717E90;
-    }
+
     th:nth-child(2), th:nth-child(4){
       min-width: 20em;
     }
@@ -74,38 +66,76 @@ const Wrapper = styled.div`
         margin-left: .25em;
     }
 @media screen and (min-width: 801px) {
-  table{
-   border-collapse: collapse;
-  }
-  td, th {
-    padding: 1.8em 2em;
-    border-top: 1px #CCC solid;
-    border-bottom: 1px #CCC solid;
-  }
+    border: 1px solid #D2D6DD;
+    background: #F9FCFF;
+    margin: 3em auto;
+    width: 75%;
+    
+    table{
+        border-collapse: collapse;
+    }
+    td, th {
+        padding: 1.8em 2em;
+        border-top: 1px #CCC solid;
+        border-bottom: 1px #CCC solid;
+        border-left: 1px solid #D2D6DD; 
+    }
+    tbody th{
+        display: none;
+    }
+    th:first-child, td:first-child{
+        border-left: none; 
+        color: #717E90;
+    }
   
 }
 @media screen and (max-width: 800px) {
+    margin: 1em auto;
+    width: 90%;
     thead {
         display: none;
     }
-
     tr {
       display: flex;
       flex-direction: column;
-      background-color: white;
-      margin: 0px 0px 20px 0px;
-      padding: 14px;
-      border: 1px #CCCCCC solid; 
+      background-color: #F9FBFF;
+      margin-bottom: 1em;
+      padding: 2em 1em;
+      border: 1px solid #DDE5F0; 
+      border-radius: .5em;
+      font-size: .9em;
     }
 
     td {
-      padding-bottom: 8px;
+        display: flex;
+        width: 100%;
+        padding-bottom: 1em;
+        font-weight: 500;
     }
-
-  /* Add labels for preview table */
-    .preview td::before {
-      content: attr(data-label) ": ";
-      font-weight: bold;
+    .index{
+        display: none;
+    }
+    
+    tbody th{
+        min-width: 12em;
+        max-width: 12em;
+        font-size: .9em;
+    }  
+    .best-deal{
+        padding-top: 0;
+        overflow: hidden;
+        &:before{
+            content: 'Лучшее придложение';
+            background: #6BC028;
+            padding: .8em 0;
+            width: calc(100% + 2em);
+            margin-left: -1em;
+            text-align: center;
+            font-weight: 700;
+            color: #fff;
+            font-size: 1.2em;
+            margin-bottom: 1.5em;
+        }
     }
 }
 `;
@@ -145,54 +175,128 @@ const Table = () =>
             </thead>
             <tbody>
             <tr>
-                <td className="name" data-label="">1</td>
-                <td className="name" data-label="Метод поиска">
-                    Самостоятельно в интернете
+                <td className="index">1</td>
+                <td className="name">
+                    <th>
+                        <div className="row">
+                            <Search/>
+                            Метод поиска
+                        </div>
+                    </th>
+                    <span>
+                        Самостоятельно в интернете
+                    </span>
                 </td>
-                <td className="id" data-label="Время поиска">
+                <td className="id">
+                    <th>
+                        <div className="row">
+                            <Time/>
+                            Время поиска
+                        </div>
+                    </th>
                     <div className="row">
                         1 день - <span className='bold infinity'>&#8734;</span>
                     </div>
                 </td>
-                <td className="type" data-label="Себестоимость">
-                    0$
+                <td className="type">
+                    <th>
+                        <div className="row">
+                            <img src="/img/card.png" alt=""/>
+                            Себестоимость
+                        </div>
+                    </th>
+                    <span>0$</span>
                 </td>
-                <td className="value" data-label="Вероятность заключить выгодную сделку">
-                    = <span className='medium'>50%</span>
+                <td className="value">
+                    <th>
+                        <div className="row">
+                            <img src="/img/start.png" alt=""/>
+                            Вероятность заключить выгодную сделку
+                        </div>
+                    </th>
+                    <span>= <span className='medium'>50%</span></span>
                 </td>
             </tr>
 
-            <tr>
-                <td className="x" data-label="">
+            <tr className='best-deal'>
+                <td className="index" data-label="">
                     2
                 </td>
-                <td className="name" data-label="Метод поиска">
-                    Вместе с <span className='blue bold'>INTA-ICS</span>
+                <td className="name">
+                    <th>
+                        <div className="row">
+                            <Search/>
+                            Метод поиска
+                        </div>
+                    </th>
+                    <span>Вместе с <span className='blue bold'>INTA-ICS</span></span>
                 </td>
-                <td className="id" data-label="Время поиска">
-                    От <span className='blue medium'>7</span> до <span className='blue medium'>14 дней</span>
+                <td className="id">
+                    <th>
+                        <div className="row">
+                            <Time/>
+                            Время поиска
+                        </div>
+                    </th>
+                    <span>От <span className='blue medium'>7</span> до <span className='blue medium'>14 дней</span></span>
                 </td>
-                <td className="type" data-label="Себестоимость">
-                    От <span className='green'>100$</span>
+                <td className="type">
+                    <th>
+                        <div className="row">
+                            <img src="/img/card.png" alt=""/>
+                            Себестоимость
+                        </div>
+                    </th>
+                    <span>От <span className='green'>100$</span></span>
                 </td>
-                <td className="value" data-label="Вероятность заключить выгодную сделку">
+                <td className="value">
+                    <th>
+                        <div className="row">
+                            <img src="/img/start.png" alt=""/>
+                            Вероятность заключить выгодную сделку
+                        </div>
+                    </th>
                     <div className='green'>90% <ArrowLine/></div>
                 </td>
             </tr>
 
             <tr>
-                <td className="x" data-label="">3</td>
-                <td className="name" data-label="Метод поиска">
-                    Поездка в Китай
+                <td className="index" data-label="">3</td>
+                <td className="name">
+                    <th>
+                        <div className="row">
+                            <Search/>
+                            Метод поиска
+                        </div>
+                    </th>
+                    <span>Поездка в Китай</span>
                 </td>
-                <td className="id" data-label="Время поиска">
-                    Около <span className='blue medium'>30 дней </span>
-                    (с учетом времени на подготовку)
+                <td className="id">
+                    <th>
+                        <div className="row">
+                            <Time/>
+                            Время поиска
+                        </div>
+                    </th>
+                    <span>Около <span className='blue medium'>30 дней </span>
+                    (с учетом времени на подготовку)</span>
                 </td>
-                <td className="type" data-label="Себестоимость">
-                    От <span className='medium'>1500$</span>
+                <td className="type">
+                    <th>
+                        <div className="row">
+                            <img src="/img/card.png" alt=""/>
+                            Себестоимость
+                        </div>
+                    </th>
+                    <span>От <span className='medium'>1500$</span></span>
                 </td>
-                <td className="value" data-label="Вероятность заключить выгодную сделку">
+                <td className="value" >
+                    <th>
+                        <div className="row">
+                            <img src="/img/start.png" alt=""/>
+                            Вероятность заключить выгодную сделку
+                        </div>
+                    </th>
                     <div className='green'>90% <ArrowLine/></div>
                 </td>
             </tr>
