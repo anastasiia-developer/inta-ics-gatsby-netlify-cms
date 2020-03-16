26fa5dfae36f3850c5722ebf196a7c23a91ab27b
Anastasiia Derymarko <lolololo2263@gmail.com>
2020-03-16 15:53:26 +0200

    del emation

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,6 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
-    `gatsby-plugin-emotion`,
     `gatsby-plugin-styled-components`,
     {
       resolve: "gatsby-plugin-react-svg",
