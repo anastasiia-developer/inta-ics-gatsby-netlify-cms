344dc024ff81ace0aea600474c3b6de5c343547d
Anastasiia Derymarko <lolololo2263@gmail.com>
2020-03-24 21:26:41 +0800

    del dubl calculate

--- src/pages/cost/index.js
+++ src/pages/cost/index.js
@@ -46,7 +46,6 @@ export const CostPageTemplate = ({
                         :
                             <CalculatorRu/>
                     }
-                    <Calculator/>
                     <BarBckgImgSectionText
                         image={barBckgImg}
                     />
