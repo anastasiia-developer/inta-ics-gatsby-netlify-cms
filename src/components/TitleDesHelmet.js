b8fab45d56ae95a16130dea0dc7bc0ef9cf51bd0
Anastasiia Derymarko <lolololo2263@gmail.com>
2020-04-02 14:58:11 +0800

    title helmet add logo

--- src/components/TitleDesHelmet.js
+++ src/components/TitleDesHelmet.js
@@ -2,13 +2,16 @@ import React from 'react'
 import Helmet from "react-helmet"
 import {withPrefix} from "gatsby";
 
-const TitleDesHelmet = ({title, description, icon}) =>
+const TitleDesHelmet = ({title, description, img, icon}) =>
     <Helmet titleTemplate="%s">
         <title>{title}</title>
         <meta
             name="description"
             content={description}
         />
+        <meta
+            property="og:image"
+            content={`${withPrefix('/')}img/logo.ico`}/>
         {icon &&
             <link
                 rel="icon"
