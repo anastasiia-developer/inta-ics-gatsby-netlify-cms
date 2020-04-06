16a570a2cbcadd1fd138a47ba5d19af5702ff85e
Anastasiia Derymarko <lolololo2263@gmail.com>
2020-04-06 21:14:41 +0800

    valid phone input

--- src/components/FormComponents/Phone.js
+++ src/components/FormComponents/Phone.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import InputMask from 'react-input-mask';
 
-const Phone = (props) =>
-    <InputMask {...props}
-        placeholder={props.placeholder || '+38 (0__) ___-__-__'}
-        mask="+38 (099) 999-99-99" />
+const Phone = (props) => {
+    const [valid, setValid] = useState(false);
+
+    return (
+        <InputMask {...props}
+            placeholder={props.placeholder || '+38 (0__) ___-__-__'}
+            mask="+38 (099) 999-99-99"
+            style={{border: `${valid ? "1px solid red" : ""}`}}
+            onBlur={(e) => setValid(e.target.value.length < 1 || e.target.value.includes("_"))}
+        />
+    )
+};
 
 export default Phone;
